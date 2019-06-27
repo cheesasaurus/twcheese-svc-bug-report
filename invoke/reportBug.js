40562a0f0306967d2d72665893fdef253c32665f
Nicholas Toby <cheesasaurus@gmail.com>
2019-06-27 06:22:22 -0500

    BugTracker

--- invoke/reportBug.js
+++ invoke/reportBug.js
@@ -1,21 +1,7 @@
-const { fetch } = require('../src/Network');
+const BugTracker = require('../src/BugTracker');
 
-
-let root = 'https://api.github.com';
-let owner = 'cheesasaurus';
-let repo = 'twcheese-bug-report'; // private repo. https://help.github.com/en/articles/creating-an-issues-only-repository
-let token = process.env.TWCHEESE_BUGREPORT_TOKEN;
 
 (async function() {
-    let response = await fetch(`${root}/repos/${owner}/${repo}/issues`, {
-        method: 'POST',
-        headers: {
-            Authorization: `token ${token}`
-        },
-        body: JSON.stringify({
-            title: 'test title',
-            body: 'test message'
-        })
-    });
-    console.log(response.json());
+    let data = await BugTracker.createIssue('test title', 'test message');
+    console.log(data);
 })();
---
+++ src/BugTracker.js
@@ -0,0 +1,27 @@
+const { fetch } = require('./Network');
+
+let BugTracker = {
+
+    async createIssue(title, message) {
+        // private repo. https://help.github.com/en/articles/creating-an-issues-only-repository
+        let url = 'https://api.github.com/repos/cheesasaurus/twcheese-bug-report/issues';
+
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                Authorization: `token ${process.env.TWCHEESE_BUGREPORT_TOKEN}`,
+                'User-Agent': 'TwCheese Bug Reporter'
+            },
+            body: JSON.stringify({
+                title: title,
+                body: message
+            })            
+        });
+        let obj = await response.json();
+        return { url: obj.html_url };
+    }
+
+};
+
+
+module.exports = BugTracker;
